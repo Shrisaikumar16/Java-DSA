abae032c581392286c4780502faba982ac9723aa
Shrisaikumar16 <shrisai225@gmail.com>
2025-12-25 00:08:40 +0530

    Add files via upload

---
+++ JavaBasics/InputSum.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class InputSum {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a+b;
+        int prod = a*b;
+        System.out.println(sum);
+        System.out.println(prod);
+        
+
+        //Area of a Circle
+        float rad = sc.nextFloat();
+        float area = 3.14f * rad * rad;
+        System.out.println(area);
+        sc.close();
+    }
+    
+}

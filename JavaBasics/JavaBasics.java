fe44570b750377f4bb0efcda92469cd5e40cf73e
Shrisaikumar16 <shrisai225@gmail.com>
2025-12-25 22:42:34 +0530

    Add files via upload

---
+++ JavaBasics/JavaBasics.java
@@ -0,0 +1,11 @@
+//Boiler Plate
+public class JavaBasics {
+    public static void main(String args[]) {
+
+        // 3 ways to print: 1) print 2) println 3) \n
+        System.out.print("Hello World\n");
+        System.out.println("Hello World");
+        System.out.print("Hello World\n");
+
+    }
+}
---
+++ JavaBasics/SumofAandB.java
@@ -0,0 +1,9 @@
+public class SumofAandB {
+    public static void main (String args[]) {
+        /*To calculate the sum */
+        int a = 10;
+        int b = 5;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}

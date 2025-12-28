f7119711bc83e72048fd20c9c72507b59f79969a
Shrisaikumar16 <shrisai225@gmail.com>
2025-12-28 14:38:15 +0530

    Add files via upload

---
+++ Operators/ArithOperator.java
@@ -0,0 +1,12 @@
+// import java.util.*;
+public class ArithOperator {
+    public static void main(String args[]) {
+        int A = 10;
+        int B = 5;
+        System.out.println("modulo = " + (A%B));
+        System.out.println("addition = " + (A+B));
+        System.out.println("subtraction = " + (A-B));
+        System.out.println("multiplication = " + (A*B));
+        System.out.println("divition = " + (A/B));
+    }
+}

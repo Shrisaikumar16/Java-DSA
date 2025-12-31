7b2c684064071442feee0b6192f6c4d68fefa635
Shrisaikumar16 <shrisai225@gmail.com>
2025-12-31 18:57:25 +0530

    Add files via upload

---
+++ Conditional_Statements/switchStatement.java
@@ -0,0 +1,21 @@
+
+public class switchStatement {
+    public static void main(String args[]) {
+        int number = 1;
+        switch(number) {
+            case 1:
+                System.out.println("samosa");
+                break;
+            case 2:
+                System.out.println("burger");   
+                break;
+            case 3:
+                System.out.println("pizza");
+                break;
+            default:
+                System.out.println("Invalid choice");
+                break;
+
+        }
+    }
+}

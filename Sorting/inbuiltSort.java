a13c8ffb2c7224bdf18bf52532e184828fc99573
Shrisaikumar16 <shrisai225@gmail.com>
2026-02-12 18:42:17 +0530

    Add files via upload

---
+++ Sorting/inbuiltSort.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class inbuiltSort {
+
+public static void printArr(Integer arr[]) {
+        for (int i=0; i<arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        Integer arr[] = {5, 4, 1, 3, 2};
+        //reverse order k liye use Collections.reverseOrder() and also declare variable as "Integer" rather tah "int"
+        Arrays.sort(arr, Collections.reverseOrder());
+        printArr(arr);
+    }
+}

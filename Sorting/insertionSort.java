966cc32895ddbfa4d1aacb5b88ac646283d490ec
Shrisaikumar Sonari <shrisai225@gmail.com>
2026-02-19 23:20:16 +0530

    Add files via upload

---
+++ Sorting/insertionSort.java
@@ -0,0 +1,28 @@
+public class insertionSort {
+
+    public static void insertSort(int arr[]) {
+        for (int i=1; i<arr.length; i++) {
+            int curr = arr[i];
+            int prev = i-1;
+            //finding out the correct psoition to insert
+            while (prev >=0 && arr[prev] > curr) {
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            //insertion
+            arr[prev+1] = curr;
+        }
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i=0; i<arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String args[]) {
+        int arr[] = {5, 4, 1, 3, 2};
+        insertSort(arr);
+        printArr(arr);
+    }
+}
